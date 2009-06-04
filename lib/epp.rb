fcdba584212029a94caf4a545b5beadab32306a0
Joshua Delsman <jdelsman@voxxit.com>
2009-06-04 14:26:02 +0100

    Updated require lines for .rb

--- lib/epp.rb
+++ lib/epp.rb
@@ -1,16 +1,18 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+# Gem and other dependencies
 require 'rubygems'
 require 'openssl'
 require 'socket'
 require 'activesupport'
-require 'require_parameters'
 require 'rexml/document'
 require 'hpricot'
 
-require 'epp/server'
-require 'epp/exceptions'
+# Package files
+require 'require_parameters.rb'
+require 'epp/server.rb'
+require 'epp/exceptions.rb'
 
 module Epp #:nodoc:
   VERSION = '1.0.3'
